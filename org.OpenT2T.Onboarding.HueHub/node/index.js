2686f6c6ce7d0f5a55043c6e181c2ad91b1f418c
Mary Chau <marcha@microsoft.com>
2016-09-17 03:20:34 +0800

    Changing to remote authentication.

--- org.OpenT2T.Onboarding.HueHub/node/index.js
+++ org.OpenT2T.Onboarding.HueHub/node/index.js
@@ -1,121 +1,246 @@
 'use strict';
 
-var hue = require("node-hue-api");
-var HueApi = require("node-hue-api").HueApi;
+/* eslint no-unused-vars: "off" */
 var inquirer = require('inquirer');
+var open = require('open');
+var express = require('express');
+var randomstring = require('randomstring');
+var https = require('follow-redirects').https;
+var crypto = require('crypto');
 
-var LightInfo = function(uniqueid, name)
-{
-	this.uniqueId = uniqueid;
-	this.name = name;
-};
-
-var getLights = function(ipaddr, userId, callback) {
-    var api = new HueApi(ipaddr, userId);
-	var lights = [];
-    api.lights(function(err, result) {
-        if (err)
-            console.log("api.lights " + err);
-        else {
-            for (var light of result.lights) {
-				var lightInfo = new LightInfo(light.uniqueid, light.name);
-                lights.push(lightInfo);
-            }
-			callback(lights);
-            return;
-        }
-    });
-};
-
-var onboardLights = function(ipaddr, userId, name, successCallback, errorCallback) {
-    getLights(ipaddr, userId, function(lights) {
-        if (!!lights && lights.length > 0) {
-
-            // If a name was specified, filter the list of lights by matching the name.
-            if (name) {
-                var nameRegexp = new RegExp(name);
-                lights = lights.filter(function (light) {
-                    return nameRegexp.test(light.name);
-                });
-            }
-
-            if (lights.length == 1) {
-                // A single light was found.
-                var selectedLight = lights[0];
-                if (successCallback) {
-                    successCallback(ipaddr, userId, selectedLight.uniqueId, 'All done. Happy coding!');
-                }
-            } else if (lights.length > 1) {
-                // More than one lights were found. Ask the user to select a light.
-                var choices = lights.map(function (light) {
-                    return JSON.stringify(light);
-                });
-                inquirer.prompt([
-                    {
-                        type: "list",
-                        name: "selectedLight",
-                        message: "Which light do you want to onboard?",
-                        choices: choices
-                    }
-                ], function(answers) {
-                    // All done. Now we have all the parameters we needed.
-                    var selectedLight = JSON.parse(answers.selectedLight);
-                    if (successCallback) {
-                        successCallback(ipaddr, userId, selectedLight.uniqueId, 'All done. Happy coding!');
-                    }
-                });
-            } else {
-                if (errorCallback) {
-                    errorCallback('NotFound', 'Some lights were found, but none matched the specified name.');
-                }
-            }
-        } else {
-            if (errorCallback) {
-                errorCallback('NotFound', 'No lights were found.');
-            }
-        }
-    });
+var config = {
+    port: process.env.PORT || 8080,
+    code_uri: 'https://api.meethue.com',
+    access_token_uri: 'api.meethue.com',
+    data_model_uri: 'api.meethue.com'
 }
 
 // module exports, implementing the schema
 module.exports = {
 
-    onboard: function(name, userId, successCallback, errorCallback) {
-        console.log('Onboarding device : ' + name);
+    onboard: function(successCallback, errorCallback) {
+        // build questions for credentials
+        console.log('\nPlease enter credentials for the Hue API:\n');
 
-        // Find a Hue bridge
-        hue.nupnpSearch(function(upnpErr, result) {
-            var hueBridgeInfo = result[0];
-            var ipaddr = hueBridgeInfo.ipaddress;
-            console.log("Found Hue bridge at " + ipaddr);
-
-            if (userId) {
-                // A user ID was specified. Go ahead and query the lights.
-                onboardLights(ipaddr, userId, name, successCallback, errorCallback);
-            } else {
-                // Ask the user to press the button, which enables creating a new user ID.
-                console.log("Important! Press the button on the top of the Hue.")
-                var readline = require('readline');
-                var rl = readline.createInterface(process.stdin, process.stdout);
-                rl.setPrompt('Then press Enter here within 30 seconds...');
-                rl.prompt();
-                rl.on('line', function() {
-                    var hueapi = new HueApi();
-                    hueapi.createUser(ipaddr, function(err, userId) {
-                        if (err) {
-                            if (errorCallback) {
-                            errorCallback('Create User Error', JSON.stringify(err));
-                            return;
-                            }
-                        } else {
-                            console.log("Created User: %s", userId);
-
-                            // Now user the userId to get the lights attached to the Hue
-                            onboardLights(ipaddr, userId, name, successCallback, errorCallback);
-                        }
-                    });
-                });
+        var questions = [
+            {
+                type: 'input',
+                name: 'client_id',
+                message: 'Hue API Client ID: ',
+                validate: function(value) {
+                    var pass = !!value;
+                    if (pass) {
+                        return true;
+                    } else {
+                        return 'Please enter a valid Client ID.';
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'client_secret',
+                message: 'Hue API Client Secret: ',
+                validate: function(value) {
+                    var pass = !!value;
+                    if (pass) {
+                        return true;
+                    } else {
+                        return 'Please enter a valid Client Secret.';
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'device_id',
+                message: 'Your Device Name: ',
+                validate: function(value) {
+                    var pass = !!value;
+                    if (pass) {
+                        return true;
+                    } else {
+                        return 'Please enter a valid Device Name.';
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'app_name',
+                message: 'Your App Name: ',
+                validate: function(value) {
+                    var pass = !!value;
+                    if (pass) {
+                        return true;
+                    } else {
+                        return 'Please enter a valid App Name.';
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'app_id',
+                message: 'Your App ID (Obtained from Hue): ',
+                validate: function(value) {
+                    var pass = !!value;
+                    if (pass) {
+                        return true;
+                    } else {
+                        return 'Please enter a valid App ID.';
+                    }
+                }
             }
+            
+        ];
+
+        // show questions for credentials
+        inquirer.prompt(questions, function(answers) {
+            console.log('\nThanks! Signing you in to Hue.');
+            
+            var stateCode = randomstring.generate().substr(0,6);
+            var authRequestUrl = 'https://api.meethue.com/oauth2/auth?';
+            authRequestUrl += 'clientid=' + answers.client_id + '&';
+            authRequestUrl += 'state=' + stateCode + '&';
+            authRequestUrl += 'deviceid=' + answers.device_id + '&';
+            authRequestUrl += 'devicename=' + 'OpenT2T' + '%20on%20' + answers.device_id + '&';
+            authRequestUrl += 'appid=' + answers.app_id + '&';
+            authRequestUrl += 'response_type=code';
+
+            var app = express();
+
+            app.get('/WindowsAtHome', function(req, res) {
+                if(req.query.state == stateCode){
+
+                    var tokenRequestURL = "/oauth2/token?";
+                    tokenRequestURL += "code=" + req.query.code + "&";
+                    tokenRequestURL += "grant_type=authorization_code";
+                    
+                    var options = {
+                        'method': 'POST',
+                        'hostname': 'api.meethue.com',
+                        'port': null,
+                        'path': tokenRequestURL
+                    };
+                    
+                    var postReq = https.request(options, function(postRes) {
+                        var buffer = '';
+                        postRes.setEncoding('utf8');
+                        postRes.on('data', function(data) {
+                            buffer += data;
+                        });
+                        
+                        postRes.on('end', function() {
+                            if (postRes.statusCode != 401) {
+                                if (errorCallback) {
+                                    errorCallback(postRes.statusCode, body);
+                                    return;
+                                }
+                            }
+                            else {
+                                // signed in, now enumerate devices and let the user pick one
+                                console.log('Signed in to Hue!');
+                                res.send('<html><body><h1>Signed in. Please close this window and return to node.</h1></body></html>');
+                                
+                                //Get nonce
+                                var digestHeader = postRes.headers['www-authenticate'];
+                                var nonce = digestHeader.substr(digestHeader.indexOf('nonce=\"')+7, 32);
+                                
+                                //Compute Digest Header
+                                var disgestHeaderContent = 'username=\"' + answers.client_id + '\", ';
+                                disgestHeaderContent += 'realm=\"oauth2_client@api.meethue.com\", ';
+                                disgestHeaderContent += 'nonce=\"' + nonce + '\", ';
+                                disgestHeaderContent += 'uri=\"/oauth2/token\", ';
+                                var HASH1 = crypto.createHash('md5').update(answers.client_id + ':oauth2_client@api.meethue.com:' + answers.client_secret).digest('hex');
+                                var HASH2 = crypto.createHash('md5').update('POST:/oauth2/token').digest('hex');
+                                var authHeaderResponse = crypto.createHash('md5').update(HASH1 + ':' + nonce + ':' + HASH2).digest('hex');
+                                disgestHeaderContent += 'response=\"' + authHeaderResponse + '\"';
+                                
+                                options.headers = {
+                                        'Accept': 'application/json',
+                                        'Authorization': 'Digest ' + disgestHeaderContent,
+                                };
+                                
+                                console.log('Responding to Digest Authentication to get access token:');
+                                console.log(options);
+                                var accessTokenReq = https.request(options, function(accessTokenRes) {
+                                    
+                                    /*
+                                    Getting an error after sending digest response:
+                                    events.js:141
+                                          throw er; // Unhandled 'error' event
+                                          ^
+
+                                    Error: socket hang up
+                                        at createHangUpError (_http_client.js:213:15)
+                                        at TLSSocket.socketOnEnd (_http_client.js:305:23)
+                                        at emitNone (events.js:72:20)
+                                        at TLSSocket.emit (events.js:166:7)
+                                        at endReadableNT (_stream_readable.js:921:12)
+                                        at nextTickCallbackWith2Args (node.js:442:9)
+                                        at process._tickCallback (node.js:356:17) 
+                                    */
+                                    
+                                    var body = '';
+                                    accessTokenRes.on('data', function(data) {
+                                        console.log(data);
+                                        body += data;
+                                    });
+
+                                    accessTokenRes.on('end', function() {
+                                        console.log(body.toString());
+                                        
+                                        if (accessTokenRes.statusCode != 200) {
+                                            if (errorCallback) {
+                                                errorCallback(accessTokenRes.statusCode, body);
+                                                return;
+                                            }
+                                        } else {
+
+                                            // retrieve access tokens
+                                            var tokens = JSON.parse(body);
+                                            if (successCallback) {
+                                                successCallback(tokens.access_token, tokens.access_token_expires_in, tokens.refresh_token, 'All done. Happy coding!');
+                                                return;
+
+                                            }
+                                            
+                                            //TODO: get remote bridge ID and whitelist ID
+                                        }
+                                    });
+
+                                    accessTokenRes.on('error', function(e) {
+                                        if (errorCallback) {
+                                            errorCallback('DigestAuth', e.message);
+                                            return;
+                                        }
+                                    });
+                                });
+                            }
+                        });
+                        
+                        postReq.on('error', function(e) {
+                            if (errorCallback) {
+                                errorCallback('GetNonce', e.message);
+                                return;
+                            }
+                        });
+                    });
+
+                    postReq.end();
+                    
+                } else {
+                    //TODO: errorCallback
+                    console.log('Error in auth request: Mismatched state code in redirect url.');
+                    if (errorCallback) {
+                        errorCallback('AuthRequest', "Mismatched state code in redirect url");
+                        return;
+                    }
+                }
+            });
+
+            // start the server which shows UX in a local browser
+            app.listen(config.port, function() {
+                console.log('Server running on port', config.port);
+                open(authRequestUrl); // open the oauth UI
+            });
         });
     }
 };
--- org.OpenT2T.Onboarding.HueHub/node/test.js
+++ org.OpenT2T.Onboarding.HueHub/node/test.js
@@ -1,17 +1,11 @@
 var huehub = require('./index');
 
-var argv = require('optimist')
-    .usage('Usage: $0 -n [lamp name] -u [user id]')
-    .default({ n : '', u : '' })
-    .argv;
-
-// register success and error callbacks for testing purposes (these are normally populated by the runtime)
-function onSuccess(ipAddress, userId, uniqueId, message) {
-    console.log("  ipAddress : " + ipAddress);
-    console.log("  userId    : " + userId);
-    console.log("  uniqueId  : " + uniqueId);
-    console.log("  message   : " + message);
-
+// register success and error callbacks for testing purposes (these are normally populated by the runtime)    
+function onSuccess(access_token, id, message) {
+    console.log("  access_token : " + access_token);
+    console.log("  id           : " + id);
+    console.log("  message      : " + message);
+    
     process.exit();
 }
 
@@ -21,5 +15,4 @@ function onError(type, message) {
 }
 
 // Call onboarding with provided parameters (this is normally called by the runtime when the user initiates onboarding)
-huehub.onboard(argv.n, argv.u, onSuccess, onError);
-
+huehub.onboard(onSuccess, onError);
