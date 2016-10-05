824292917050b95652b372d79761e5490036ae5f
m-chau <m-chau@users.noreply.github.com>
2016-10-05 19:39:58 +0800

    Update test.js

--- org.opent2t.onboarding.hue/js/tests/test.js
+++ org.opent2t.onboarding.hue/js/tests/test.js
@@ -18,7 +18,7 @@ test.serial('onboard', t => {
             // TEST: translator is valid
             t.is(typeof onboarding, 'object') && t.truthy(onboarding);
 
-            return OpenT2T.invokeMethodAsync(onboarding, "org.opent2t.onboarding.nesthub", "onboard", [testConfig])
+            return OpenT2T.invokeMethodAsync(onboarding, "org.opent2t.onboarding.huehub", "onboard", [testConfig])
                 .then((accessToken) => {
                     console.log("accessToken:");
                     console.log(JSON.stringify(accessToken, null, 2));
